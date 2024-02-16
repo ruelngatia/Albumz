715bb5a19d592d0eb0ea5ceed339af9d818d80f2
ruelngatia <ruelngatia@gmail.com>
2024-02-16 12:25:09 +0300

    added required packages and also setup tailwind css

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      purple: '#8800FE',
+      fadePurple: '#9419FE',
+      hoverPurple: '#72568b',
+      dullPurple: '#302644',
+      white: '#FFFFFF',
+      wheat: '#FAF5FF',
+      footerGrey: '#EDE4F3',
+      grey: '#D9D9D9',
+      greyUser: '#F1F1F1'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
